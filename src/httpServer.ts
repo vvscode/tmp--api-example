859b3a7c6bf6eef3def705e9c1c265ed5fcb816d
Vasil Vanchuk <v.vanchuk@tinkoff.ru>
2023-12-13 21:32:28 +0300

    server static files

--- src/httpServer.ts
+++ src/httpServer.ts
@@ -1,8 +1,29 @@
 import http from 'node:http';
+const { URL } = require('node:url');
+import fs from 'node:fs';
+import util from 'node:util';
+import path from 'node:path';
 
-const server = http.createServer((req, res) => {
-    res.write('Hello bro 222' as string);
-    res.end();
+const readFile = util.promisify(fs.readFile);
+const exists = util.promisify(fs.exists);
+
+
+
+const server = http.createServer(async (req, res) => {
+    const url = new URL(`http://${req.headers.host}${req.url}`);
+
+    const pathNameWithoutLeadingSlash = url.pathname.replace(/^\/+/, '');
+    const pathname = path.resolve(__dirname, '../public', pathNameWithoutLeadingSlash || 'index.html');
+
+    if (await exists(pathname)) {
+        const data = await readFile(pathname);
+        res.write(data);
+        res.end();
+    } else {
+        res.writeHead(404);
+        res.write(JSON.stringify({status: 'not found'}))
+        res.end();
+    }
 });
 
 
