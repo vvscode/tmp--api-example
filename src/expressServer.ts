40b42e1cd751892c5a0526c2a15008e35f1a18c2
Vasil Vanchuk <v.vanchuk@tinkoff.ru>
2023-12-14 00:10:40 +0300

    adjust settings for vercel

--- src/expressServer.ts
+++ src/expressServer.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import session from 'express-session';
 import body from 'body-parser';
+import { glob } from 'glob';
+import path from 'node:path';
 
 const port = process.env.PORT || 3000;
 
@@ -37,6 +39,17 @@ app.post('/msg', jsonBody, (req, res) => {
     req.session.message = String(req.body.msg);
     res.send('Thanks');
 });
+
+app.get('/list', (req, res) => {
+    let cwdList = glob.sync('**/**', {
+        ignore: ['node_modules/**'] 
+    });
+    let dirList = glob.sync('**/**', {
+        cwd: __dirname + '/..',
+        ignore: ['node_modules/**'] 
+    })
+    res.send({ cwdList, dirList, __dirname: path.resolve(__dirname, '..'), cwd: process.cwd(),views: app.get('views') });
+})
 
 app.get('/msg', (req, res) => {
     res.render('message', { lastMessage: String(req.session.message), cwd: process.cwd(), __dirname });
